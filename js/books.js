cd2c056bedab9d57e0cafb2837229759300acce6
Ryan Nelson <iamryandnelson@gmail.com>
2025-11-25 16:21:19 -0800

    update

--- js/books.js
+++ js/books.js
@@ -2125,6 +2125,17 @@ const booksData = [
         genre: ["Self Help", "usiness", "Nonfiction", "Personal Development", "Productivity", "Finance", "Psychology"]
     },
     {
+        isbn: 9780593652886,
+        title: "The Creative Act: A Way of Being",
+        overview: "Many famed music producers are known for a particular sound that has its day. Rick Rubin is known for something else: creating a space where artists of all different genres and traditions can home in on who they really are and what they really offer. He has made a practice of helping people transcend their self-imposed expectations in order to reconnect with a state of innocence from which the surprising becomes inevitable. Over the years, as he has thought deeply about where creativity comes from and where it doesn't, he has learned that being an artist isn't about your specific output, it's about your relationship to the world. Creativity has a place in everyone's life, and everyone can make that place larger. In fact, there are few more important responsibilities. <p> The Creative Act is a beautiful and generous course of study that illuminates the path of the artist as a road we all can follow. It distills the wisdom gleaned from a lifetime's work into a luminous reading experience that puts the power to create moments--and lifetimes--of exhilaration and transcendence within closer reach for all of us.",
+        pages: "406",
+        author: "Rick Rubin",
+        release_date: "January 17, 2023",
+        wikipedia: "Rick_Rubin",
+        poster: "img/the_creative_act.jpg",
+        genre: ["Nonfiction", "Art", "Self Help", "Philosophy", "Music", "Writing"]
+    },
+    {
         isbn: 9780062512666,
         title: "The Cube",
         overview: "Describes an ancient visualization game that provides insight into the understanding of the self and relationships",
